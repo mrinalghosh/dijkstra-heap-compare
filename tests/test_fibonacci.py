9ab70a0d72e6013c39a68df6057d0a6a43d73bfd
Panat Taranat <panat.taranat@gmail.com>
2021-04-29 00:43:26 -0400

    Update tests to use node dict syntax for insert

--- tests/test_fibonacci.py
+++ tests/test_fibonacci.py
@@ -8,46 +8,47 @@ def uut():
 
 
 def test_insert(uut):
-    test_node = uut.insert(5, "apples")
+    node = {"key": 5, "value": "apples"}
+    test_node = uut.insert(node, node["value"])
     assert test_node.key == 5
-    assert test_node.value == "apples"
+    assert test_node.name == "apples"
 
 
 def test__find_min_node(uut):
-    uut.insert(8)
-    uut.insert(7)
-    uut.insert(-9)
-    uut.insert(4)
+    uut.insert({"key": 8})
+    uut.insert({"key": 7})
+    uut.insert({"key": -9})
+    uut.insert({"key": 4})
     assert uut._find_min_node().key == -9
 
 
 def test_find_min(uut):
-    uut.insert(5)
-    uut.insert(4)
-    uut.insert(7)
-    uut.insert(9)
-    uut.insert(67)
+    uut.insert({"key": 5})
+    uut.insert({"key": 4})
+    uut.insert({"key": 7})
+    uut.insert({"key": 9})
+    uut.insert({"key": 67})
     uut.extract_min()  # Extract 4
     uut.extract_min()  # Extract 5
     assert uut.find_min().key == 7
 
 
 def test_extract_min(uut):
-    uut.insert(5)
-    uut.insert(4)
-    uut.insert(7)
-    uut.insert(9)
-    uut.insert(67)
+    uut.insert({"key": 5})
+    uut.insert({"key": 4})
+    uut.insert({"key": 7})
+    uut.insert({"key": 9})
+    uut.insert({"key": 67})
     assert uut.extract_min().key == 4
     assert uut.extract_min().key == 5
 
 
 def test_decrease_key(uut):
-    uut.insert(5)
-    uut.insert(4)
-    x = uut.insert(100)
-    y = uut.insert(9)
-    uut.insert(67)
+    uut.insert({"key": 5})
+    uut.insert({"key": 4})
+    x = uut.insert({"key": 100})
+    y = uut.insert({"key": 9})
+    uut.insert({"key": 67})
     uut.extract_min()
     uut.decrease_key(x, 3)
     assert uut.extract_min().key == 3  # extract x
@@ -56,6 +57,6 @@ def test_decrease_key(uut):
 
 
 def test_decrease_key_greater(uut):
-    x = uut.insert(5)
+    x = uut.insert({"key": 5})
     with pytest.raises(ValueError):
         uut.decrease_key(x, 10)
