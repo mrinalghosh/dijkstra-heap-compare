f3e9bad42d1b956f5d1f16953c686912a5e40abd
Panat Taranat <panat.taranat@gmail.com>
2021-04-29 01:16:07 -0400

    Add Fibonacci Heap implementation. (#10)
    * Add Fibonacci Heap implementation. Fix #1
    
    This adds two classes, FibTree and FibHeap.
    Might have to rename functions or move to new file.
    
    * Restructure the code.
    
    * Ignore VS folders
    
    * Rewrote FibHeap
    
    Instead of using FibTree, now uses Node subclass.
    Added decrease_key and other methods.
    
    * Fix min_node not updating during insert
    
    It should start off as None, not Node().
    
    * Remove FibTree
    
    * Add unit tests for pytest
    
    * Removed VS folder
    
    * Add main code for dijkstra. Working with simple heapq
    
    * Add a main function that draws shortest path using Dijkstra
    
    This should standardize the Heap interface. We use different Heap in Dijkstra algorithms like:
      heap.extract_min().value
      heap.insert(neighbor_dist, neighbor)
    
    All Heap implementations should return the Node object when extract_min().
    And all Heap implementations should insert(key, value).
    
    * Rename internal methods
    
    * Rename to match base class find_min
    
    * Rename show method and add references
    
    Reorder functions to better match reference from Rosetta Code
    
    * Update tests to use node dict syntax for insert
    
    * Update Dijkstra to use decrease_key
    
    This requires substantial changes in heap implementations.
    We are now passing nx node data as a dictionary when inserting to a heap.
    
    Resolves #1
    Resolves #14
    
    * Clean using dos2unix
    
    Co-authored-by: Amanda Justiniano-Pagán <amandaju@microsoft.com>

--- heaps/base.py
+++ heaps/base.py
@@ -1,30 +1,30 @@
-''' Abstract Base Class for a Heap Object.'''
+""" Abstract Base Class for a Heap Object."""
 
 
 class Heap(object):
-    ''' Generic heap class for inheritance '''
+    """Generic heap class for inheritance"""
 
     def __init__(self):
-        ''' initialize heap specific data structures '''
+        """initialize heap specific data structures"""
         pass
 
-    def insert(self, key, value):
-        ''' insert a new vertex into heap'''
+    def insert(self, data: dict, name=None):
+        """insert a new vertex into heap"""
 
     def extract_min(self):
-        ''' delete minimum '''
+        """delete minimum"""
 
     def find_min(self):
-        ''' return minimum '''
+        """return minimum"""
 
-    def decrease_key(self, vertex, key):
-        ''' decrease key and maintain heap '''
+    def decrease_key(self, node, k):
+        """decrease key and maintain heap"""
         pass
 
     def show(self):
-        ''' print heap in appropriate format '''
+        """print heap in appropriate format"""
         pass
 
     def __len__(self):
-        ''' return number of elements in heap'''
+        """return number of elements in heap"""
         return len(self.heap)
--- heaps/fibonacci.py
+++ heaps/fibonacci.py
@@ -1,71 +1,213 @@
-from graph_util.graph import Vertex
+from heaps.base import Heap
 
-class FibTree(Vertex):
-    '''Vertex Object for Fibonacci Heap.'''
-
-    def __init__(self, distance=float('inf'), order=0):
-        super().__init__(distance)
-        self.order = order
-
-    def append(self, t):
-        self.child.append(t)
-        self.order += 1
-
-
+# https://rosettacode.org/wiki/Fibonacci_heap#Python
 class FibHeap(Heap):
-    ''' Fibonacci heap priority queue '''
+    """Fibonacci heap priority queue"""
 
-    def __init__(self):
-        self.trees = []
-        self.least = None
-        self.count = 0
-    
-    def insert(self, value):
-        new_tree = FibTree(value)
-        self.trees.append(new_tree)
-        if (self.least is None or value < self.least.value):
-            self.least = new_tree
-        self.count += 1
+    class Node:
+        def __init__(self, data: dict, name=None):
+            self.key = data["key"]
+            self.name = name
+            self.parent = self.child = self.left = self.right = None
+            self.degree = 0
+            self.mark = False
+            self.data = data
 
-    def get_min(self):
-        if self.least is None:
-            return None
-        return self.least.value
+    # Pointer to element of doubly linked root list
+    root_list = None
+    # Pointer to node with minimum element on heap
+    min_node = None
+    # Num nodes in heap
+    total_nodes = 0
 
-    def extract_min(self):
-        smallest = self.least
+    def __len__(self):
+        return self.total_nodes
+
+    def find_min(self) -> Node:
+        return self.min_node
+
+    def insert(self, data: dict, name=None):
+        node = self.Node(data, name)
+        node.left = node.right = node
+        self._merge_with_root_list(node)
+        # Update min if needed
+        if self.min_node is None or node.key < self.min_node.key:
+            self.min_node = node
+        self.total_nodes += 1
+        return node
+
+    # Iterate through a doubly linked list
+    def _iterate(self, head=None):
+        if head is None:
+            head = self.root_list
+        current = head
+        while True:
+            yield current
+            if current is None:
+                break
+            current = current.right
+            if current == head:
+                break
+
+    def extract_min(self) -> Node:
+        smallest = self.min_node
+        if smallest is None:
+            raise ValueError("Cannot extract minimum: FibHeap is empty.")
         if smallest is not None:
-            for child in smallest.child:
-                self.trees.append(child)
-            self.trees.remove(smallest)
-            if self.trees == []:
-                self.least = None
+            children = [x for x in self._iterate(smallest.child)]
+            for i in range(0, len(children)):
+                self._merge_with_root_list(children[i])
+                children[i].parent = None
+        self._remove_from_root_list(smallest)
+        self.total_nodes -= 1
+        # Update min
+        if smallest == smallest.right:
+            self.min_node = self.root_list = None
+        else:
+            # self.min_node = smallest.right
+            self.min_node = self._find_min_node()
+            self._consolidate()
+        return smallest
+
+    # Decrease key of node in the heap in O(1)
+    def decrease_key(self, node: Node, k):
+        if k >= node.key:
+            raise ValueError("Cannot decrease key with value >= current key")
+        node.key = k
+        p = node.parent
+        if p is not None and node.key < p.key:
+            self._cut(node, p)
+            self._cascading_cut(p)
+        if node.key < self.min_node.key:
+            self.min_node = node
+
+    # Merge two trees in O(1) by concatenating root list
+    def _merge(self, fh: "FibHeap") -> "FibHeap":
+        if fh.total_nodes == 0:
+            return
+        H = FibHeap()
+        H.root_list, H.min_node = self.root_list, self.min_node
+        last = fh.root_list.left
+        fh.root_list.left = H.root_list.left
+        H.root_list.left.right = fh.root_list
+        H.root_list.left = last
+        H.root_list.left.right = H.root_list
+        # update min node if needed
+        if fh.min_node.key < H.min_node.key:
+            H.min_node = fh.min_node
+        # update total nodes
+        H.total_nodes = self.total_nodes + fh.total_nodes
+        return H
+
+    # If child node becomes smaller than parent node,
+    # cut child node off and move to  root list
+    def _cut(self, node: Node, parent: Node):
+        self._remove_from_child_list(parent, node)
+        parent.degree -= 1
+        self._merge_with_root_list(node)
+        node.parent = None
+        node.mark = False
+
+    def _cascading_cut(self, node: Node):
+        p = node.parent
+        if p is not None:
+            if p.mark is False:
+                p.mark = True
             else:
-                self.least = self.trees[0]
-                self.merge()
-            self.count -= 1
-            return smallest.value
+                self._cut(node, p)
+                self._cascading_cut(p)
 
-    def merge(self):
-        # Floor log
-        aux = (frexp(self.count)[1]) * [None]
+    # Consolidate root nodes of equal degree
+    def _consolidate(self):
+        if self.root_list is None:
+            return
+        ranks_mapping = [None] * self.total_nodes
+        nodes = [x for x in self._iterate(self.root_list)]
+        for node in nodes:
+            degree = node.degree
+            while ranks_mapping[degree] != None:
+                other = ranks_mapping[degree]
+                if node.key > other.key:
+                    node, other = other, node
+                self._heap_link(node, other)
+                ranks_mapping[degree] = None
+                degree += 1
+            ranks_mapping[degree] = node
 
-        while self.trees != []:
-            tree_x = self.trees[0]
-            order = tree_x.order
-            self.trees.remove(tree_x)
-            while aux[order] is not None:
-                tree_y = aux[order]
-                if tree_x.value > tree_y.value:
-                    tree_x, tree_y = tree_y, tree_x
-                tree_x.append(tree_y)
-                aux[order] = None
-                order += 1
-            aux[order] = x
-        
-        self.least = None
-        for k in aux:
-            if k is not None:
-                self.trees.append(k)
-                if (self.least is None or k.value < self.least.value):
-                    self.least = k
+    def _merge_with_root_list(self, node: Node):
+        if self.root_list is None:
+            self.root_list = node
+        else:
+            node.right = self.root_list.right
+            node.left = self.root_list
+            self.root_list.right.left = node
+            self.root_list.right = node
+
+    def _remove_from_root_list(self, node: Node):
+        if self.root_list is None:
+            raise ValueError("Cannot remove from empty heap")
+        if self.root_list == node:
+            if self.root_list == self.root_list.right:
+                self.root_list = None
+                return
+            else:
+                self.root_list = node.right
+        node.left.right = node.right
+        node.right.left = node.left
+        return
+
+    def _merge_with_child_list(self, parent: Node, node: Node):
+        if parent.child is None:
+            parent.child = node
+        else:
+            node.right = parent.child.right
+            node.left = parent.child
+            parent.child.right.left = node
+            parent.child.right = node
+
+    def _remove_from_child_list(self, parent: Node, node: Node):
+        if parent.child == parent.child.right:
+            parent.child = None
+        elif parent.child == node:
+            parent.child = node.right
+            node.right.parent = parent
+        node.left.right = node.right
+        node.right.left = node.left
+
+    def _heap_link(self, node: Node, other: Node):
+        self._remove_from_root_list(other)
+        other.left = other.right = other
+        # Adding other node to child list of the frst one.
+        self._merge_with_child_list(node, other)
+        node.degree += 1
+        other.parent = node
+        other.mark = False
+
+    # Iterate through list to find minimum node
+    def _find_min_node(self):
+        if self.root_list is None:
+            return None
+        else:
+            smallest = self.root_list
+            for x in self._iterate(self.root_list):
+                if x.key < smallest.key:
+                    smallest = x
+            return smallest
+
+    def show(self):
+        if self.root_list is not None:
+            count = 0
+            for heap in self._iterate():
+                print(f"tree{count}\n[")
+                self._print_tree(heap)
+                print("]\n")
+                count += 1
+
+    def _print_tree(self, node: Node):
+        if node is None:
+            return
+        print(f"Key: {node.key}")
+        if node.child is not None:
+            print()
+            for child in self._iterate(node.child):
+                self._print_tree(child)
